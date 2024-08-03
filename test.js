22f16040d83a77509ccae16664ca3ec4a1ab4f84
Trần Quốc Việt <45910205+viettq28@users.noreply.github.com>
2024-08-03 18:54:31 +0700

    Delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-function mayXayNuocMia(mia, ly, ongHut) {
-  console.log('abc');
-  return 'da co nuoc ' + mia + ', cho da vao ' + ly + ', sau do cam ' + ongHut;
-}
-
-console.log(mayXayNuocMia('cam', 'bat', 'muong'))
-
