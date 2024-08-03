b75e917538c64520beac7c480a6e617a0e728445
viettq28 <tquocviet28@gmail.com>
2024-08-03 18:49:50 +0700

    FIX

--- routes/sessionRoutes.js
+++ routes/sessionRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const sessionCtrl = require('../controllers/chatController');
+const authCtrl = require('../controllers/authController');
 
 const router = express.Router();
 
-router.get('/', sessionCtrl.getAllChatSessions);
+router.use(authCtrl.protect);
+router.get('/', authCtrl.restrictTo('admin', 'consultant'), sessionCtrl.getAllChatSessions);
 router.get('/:id', sessionCtrl.getChatSession);
 
 module.exports = router;
